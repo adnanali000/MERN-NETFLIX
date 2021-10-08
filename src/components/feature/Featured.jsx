c92756b69388d34dcfda4b927e231669ce8763aa
adnanali000 <adnan.museel123@gmail.com>
2021-10-09 02:57:34 +0500

    done with featured component

--- src/components/feature/Featured.jsx
+++ src/components/feature/Featured.jsx
@@ -23,17 +23,12 @@ export default function Featured({type}){
                         <option value="drama">Drama</option>
                         <option value="animation">Animation</option>
                         <option value="docuentary">Documentary</option>
-
-
-
-
                     </select>
                 </div>
             )}
             <img src="https://wallpaperaccess.com/full/1885334.jpg" alt="" />
             <div className="info">
                 <img src="https://occ-0-3587-58.1.nflxso.net/dnm/api/v6/tx1O544a9T7n8Z_G12qaboulQQE/AAAABfp5_exPnNUZnDZIPJsnSttBiba9tCC2-056d6YlKtS0ntsLFE4Wi2hd_fZXsbO_jXoztbMNX63wldZmThL1W6HDTyrB2PmtEO41ftM-A7sTZOzqPEqIXKKcNZ12_z6rNQTXuIMn-SY5aP80ByhYYeq-nXMSFcGyJntaHJDtm2iTCg.png?r=7c3" alt="" />
-                {/* <img src="https://i.pinimg.com/originals/99/40/e0/9940e0840029eec5e83c9974210960d9.png" alt="" /> */}
                 <span className='desc'>Eight thieves take hostages and lock themselves in the Royal Mint of Spain as a criminal mastermind manipulates the police to carry out his plan.</span>
                 <div className="buttons">
                     <button className='play'>
