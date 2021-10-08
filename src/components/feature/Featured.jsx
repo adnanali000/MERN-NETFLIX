34d2e8e8f8289487e2b4e49ff3844b7e8cfed0ce
adnanali000 <adnan.museel123@gmail.com>
2021-10-09 02:53:58 +0500

    featured compomemt of homepage

--- src/components/feature/Featured.jsx
+++ src/components/feature/Featured.jsx
@@ -1,9 +1,35 @@
 import { InfoOutlined, PlayArrow } from '@material-ui/icons'
 import './featured.scss'
 
-export default function Featured(){
+export default function Featured({type}){
     return(
+
         <div className='featured'>
+            {type && (
+                <div className="category">
+                    <span>{type == 'movie' ? 'Movies' : 'Series'}</span>
+                    <select name="genre" id="genre">
+                        <option>Genre</option>
+                        <option value="Adventure">Adventure</option>
+                        <option value="comedy">Comedy</option>
+                        <option value="crime">Crime</option>
+                        <option value="fantasy">Fantasy</option>
+                        <option value="horror">Horror</option>
+                        <option value="historical">Historical</option>
+                        <option value="romance">Romance</option>
+                        <option value="sci-fi">Sci-fi</option>
+                        <option value="thriller">Thriller</option>
+                        <option value="western">Western</option>
+                        <option value="drama">Drama</option>
+                        <option value="animation">Animation</option>
+                        <option value="docuentary">Documentary</option>
+
+
+
+
+                    </select>
+                </div>
+            )}
             <img src="https://wallpaperaccess.com/full/1885334.jpg" alt="" />
             <div className="info">
                 <img src="https://occ-0-3587-58.1.nflxso.net/dnm/api/v6/tx1O544a9T7n8Z_G12qaboulQQE/AAAABfp5_exPnNUZnDZIPJsnSttBiba9tCC2-056d6YlKtS0ntsLFE4Wi2hd_fZXsbO_jXoztbMNX63wldZmThL1W6HDTyrB2PmtEO41ftM-A7sTZOzqPEqIXKKcNZ12_z6rNQTXuIMn-SY5aP80ByhYYeq-nXMSFcGyJntaHJDtm2iTCg.png?r=7c3" alt="" />
--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -2,6 +2,7 @@ import './home.scss';
 import React from 'react'
 import Navbar from '../components/navbar/Navbar';
 import Featured from '../components/feature/Featured';
+
 const Home = () => {
     return (
         <div className='home'>
