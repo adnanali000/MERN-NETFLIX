b8b4d7bcc7e84271928d8cd58032ce69d8649560
adnanali000 <adnan.museel123@gmail.com>
2021-10-09 01:19:04 +0500

    featured component work

---
+++ src/components/feature/Featured.jsx
@@ -0,0 +1,9 @@
+import './featured.scss'
+
+export default function Featured(){
+    return(
+        <div className='featured'>
+            <img src="https://i.pinimg.com/originals/91/8a/c8/918ac8598f87e0cfd10926dd20cb6dd1.jpg" width='100%' alt="" />
+        </div>
+    )
+}
