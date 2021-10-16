b3bcc3a6d7610e4f8733090867b4353c7a596843
adnanali000 <adnan.museel123@gmail.com>
2021-10-16 11:29:54 +0500

    show trailer on hover done

--- src/components/listItem/ListItem.jsx
+++ src/components/listItem/ListItem.jsx
@@ -4,6 +4,7 @@ import {PlayArrow , Add ,ThumbUpAltOutlined, ThumbDownOutlined } from '@material
 
 const ListItem = ({index}) => {
     const [isHovered, setIsHovered] = useState(false);
+    const trailer = 'https://player.vimeo.com/external/371433846.sd.mp4?s=236da2f3c0fd273d2c6d9a064f3ae35579b2bbdf&profile_id=139&oauth2_token_id=57447761';
 
     return (
         <div className='listItem' 
@@ -12,6 +13,13 @@ const ListItem = ({index}) => {
             onMouseLeave={()=>setIsHovered(false)}
         >
             <img src = 'https://dailyresearchplot.com/wp-content/uploads/2020/11/Emily-in-Paris-1200x900.jpg' alt = '' />
+
+            {/* video  */}
+            {isHovered && (
+
+            <>
+            <video src = {trailer} autoPlay={true} loop />
+
             <div className="itemInfo">
                 <div className="icons">
                     <PlayArrow />
@@ -31,6 +39,8 @@ const ListItem = ({index}) => {
                     Action
                 </div>
             </div>
+            </>
+            )}
         </div>
     )
 }
