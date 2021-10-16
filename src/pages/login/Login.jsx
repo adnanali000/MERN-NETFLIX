718f22fedd5053c6069bdb9c41e53300637987ef
adnanali000 <adnan.museel123@gmail.com>
2021-10-16 19:57:49 +0500

    login page work completed

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -15,7 +15,17 @@ export default function Login() {
                 </div>
             </div>
             <div className="container">
-
+                <form>
+                    <h1>Sign In</h1>
+                    <input type="email" placeholder='email address' />
+                    <input type="password" placeholder='password' />
+                    <button className='loginButton'>Sign In</button>
+                    <span>New to Netflix? <b>Sign up now.</b> </span>
+                    <small>
+                        This page is protected by Google reCAPTCHA to ensure you're not a bot. 
+                        <b>Learn more</b>
+                    </small>
+                </form>
                
             </div>
         </div>
