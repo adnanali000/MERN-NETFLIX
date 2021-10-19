b7ade14d5d0228799ee68c0f9b45689804a3bc4a
adnanali000 <adnan.museel123@gmail.com>
2021-10-19 11:11:08 +0500

    set route for registration

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const authRoute = require('./routes/auth');
 
 
 //env configuratiom
@@ -10,13 +11,14 @@ dotenv.config();
 //connent to mongoose
 mongoose.connect(process.env.MONGO_URL,{
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    // useCreateIndex: true
 })
 .then(()=>console.log('Server connected successfully'))
 .catch(err=>console.log(err))
 
 
-
+app.use('/api/auth',authRoute);
 
 app.listen(8800,()=>{
     console.log('backend server running');
