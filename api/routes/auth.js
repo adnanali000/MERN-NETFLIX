7889aa8dabefc382512976a08327b08a8d00065d
adnanali000 <adnan.museel123@gmail.com>
2021-10-19 13:01:15 +0500

    login success

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -2,7 +2,6 @@ const router = require('express').Router();
 const User = require('../models/User');
 const CryptoJS = require('crypto-js');
 
-
 //register
 router.post('/register', async (req,res)=>{
     const newUser = new User({
@@ -14,7 +13,6 @@ router.post('/register', async (req,res)=>{
             process.env.SECRET_KEY
             ).toString()
     }) 
-
     try{
         const user = await newUser.save();
         res.status(201).json(user);
@@ -23,4 +21,43 @@ router.post('/register', async (req,res)=>{
     }
 })
 
+//Login
+router.post('/login', async(req,res)=>{
+    try{
+        const user = await User.findOne({email:req.body.email});
+        //if not user
+        !user && res.status(401).json('wrong password or username');
+
+        //decrypt password
+        const bytes = CryptoJS.AES.decrypt(user.password,process.env.SECRET_KEY);
+        const originalPassword = bytes.toString(CryptoJS.enc.Utf8);
+
+        originalPassword !== req.body.password && 
+            res.status(401).json('wrong password or username');
+
+
+        const {password, ...info} = user._doc;    
+        res.status(200).json(info);
+    
+    }catch(err){
+        res.status(500).json(err);
+    }
+})
+
+
 module.exports = router;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
