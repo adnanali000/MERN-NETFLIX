0c55545c094207b3a7ab9dd75c8c5f667afc6d70
adnanali000 <adnan.museel123@gmail.com>
2021-10-19 09:25:04 +0500

    backend work started

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -15,7 +15,7 @@ const Navbar = () => {
         <div className={scrolled ? 'navbar scrolled' : 'navbar'}>
             <div className="container">
                  <div className="left">
-                    <img src="https://thereelbits.com/wp-content/uploads/2017/05/Netflix-Logo.jpg" alt="" /> 
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/2560px-Netflix_2015_logo.svg.png" alt="" /> 
                 <span>Homepage</span>
                 <span>Series</span>
                 <span>Movies</span>
