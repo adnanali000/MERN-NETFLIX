83854f12eeca05e8a9d8e6ab05df0837268b628c
adnanali000 <adnan.museel123@gmail.com>
2021-10-19 12:08:11 +0500

    registration work completed

--- api/index.js
+++ api/index.js
@@ -13,7 +13,7 @@ mongoose.connect(process.env.MONGO_URL,{
     useNewUrlParser: true,
     useUnifiedTopology: true,
 })
-.then(()=>console.log('database connected successfully'))
+.then(()=>console.log('Database connected successfully'))
 .catch(err=>console.log(err))
 
 
